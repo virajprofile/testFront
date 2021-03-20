23a047afdc092e64b0defc54568665bb99080589
Viraj Shakya Samaranayake <virajofficialinfo@gmail.com>
2021-03-20 23:08:53 +0530

    test run

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+        <button>Odd</button>
+        <p>1,3,5,7,9,11,13,15,17,19</p>
+        <br></br>
+
+
+        <button>even</button>
+        <p>1,3,5,7,9,11,13,15,17,19</p>
+        <br></br>
+
+
+        <input></input>
+        <p>to</p>
+        <input></input>
+        <br></br>
+        <button>find</button>
+        <p>1,3,5,7,9,11,13,15,17,19</p>
+        <br></br>
+
+
+        <input></input>
+        <button>add</button>
+
+
+    </div>
+  );
+}
+
+export default App;
